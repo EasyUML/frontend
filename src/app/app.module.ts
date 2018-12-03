04e0a64618ce229cd626eec3ea90afd31ad2901e
Natalia Sul <natasul001@gmail.com>
2018-12-03 21:30:40 +0200

    authotization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { DialogModule } from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { SidebarModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DialogModule,
+    CheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    ConfirmDialogModule,
+    AutoCompleteModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/authorization/authorization.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization',
+  templateUrl: './authorization.component.html',
+  styleUrls: ['./authorization.component.sass']
+})
+export class AuthorizationComponent implements OnInit {
+
+display = true;
+display1 = false;
+constructor() { }
+ngOnInit() {
+}
+
+showDialog() {
+this.display = true;
+}
+ShowSignUp() {
+  this.display1 = true;
+  this.display = false;
+}
+}
